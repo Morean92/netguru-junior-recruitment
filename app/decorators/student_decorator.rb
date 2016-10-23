2e140455963a67b56331c356737b14cc72d7dcf6
Morean92 <pawel.dukarski@live.com>
2016-10-23 21:24:02 +0200

    Adding avg_notes method to Student Decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    avg = subject_item_notes.for_subject_item(subject_item).average(:value)
+    sprintf("%.2f", avg || 0) 
   end
 end
