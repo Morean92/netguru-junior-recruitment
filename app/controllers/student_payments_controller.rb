b5c40e1d836bb0738cc483102f36d40f82a482d3
Morean92 <pawel.dukarski@live.com>
2016-10-25 02:41:39 +0200

    Adding authentication to Payments

--- app/controllers/student_payments_controller.rb
+++ app/controllers/student_payments_controller.rb
@@ -1,3 +1,5 @@
 class StudentPaymentsController < ApplicationController
   expose(:student_payments) { StudentPayment.includes(:student).order(:student_id, :due_date) }
+
+  before_action :authenticate_user!
 end
